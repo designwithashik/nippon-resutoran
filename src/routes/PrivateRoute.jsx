dc0306407b5cac34f51dbec90f89787e59acc9a8
designwithashik <designwithashik@gmail.com>
2023-05-02 15:55:18 +0600

    Main Layout Added

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+    const { loading, user } = useContext(AuthContext);
+    if(loading) {
+       return <h2>Wait a bit..</h2>
+    }
+    if (user) {
+        return {children}
+    }
+    else {
+        return <Navigate to='/login'></Navigate>
+    }
+};
+
+export default PrivateRoute;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import MainLayout from '../layouts/MainLayout';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App/>
+        element: <MainLayout/>
     }
 ])
 
