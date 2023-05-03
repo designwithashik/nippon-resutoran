1cc41d221519228fbc7495e6a14f76b606627b49
designwithashik <designwithashik@gmail.com>
2023-05-03 11:35:02 +0600

    Chef Cards Added with dynamic route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Link } from 'react-router-dom';
+import { Box, Button, Flex, Heading, Image, ScaleFade, useDisclosure } from '@chakra-ui/react';
+import ChefCard from './chefs/ChefCard';
+
+/**
+ * 
+ *  https://i.ibb.co/x2DbHWH/Ellipse-1.png
+https://i.ibb.co/QrFZ8bV/png-transparent-ripe-lemons-juice-soft-drink-lemonade-fruit-lemon-natural-foods-food-dried-fruit-rem.png
+ */
 
 const Home = () => {
-    const chefs = useContext(AuthContext);
+    const bowl = 'https://i.ibb.co/x2DbHWH/Ellipse-1.png'
+    const lemon = 'https://i.ibb.co/QrFZ8bV/png-transparent-ripe-lemons-juice-soft-drink-lemonade-fruit-lemon-natural-foods-food-dried-fruit-rem.png'
+    const {chefs} = useContext(AuthContext);
     console.log(chefs)
+    const { isOpen, onToggle } = useDisclosure()
+    useEffect(() => {
+        onToggle()
+    }, [])
     return (
         <div>
-            <h2>Alhamdulillah</h2>
-            <Link to={`chef/1003`}>Chef Recipes</Link>
+
+            <Flex>
+
+                <Box ml='83px' mb='102px' mt='41px'>
+                    <ScaleFade initialScale={2.9} in={isOpen}>
+                        <Image src={bowl}></Image>
+                    </ScaleFade>
+                </Box>
+
+                <Flex ml='auto' mr='164px' flexDir='column' gap='10px' justifyContent='center'>
+                    <Heading >TASTE OUR</Heading><Heading>DELICIOUS</Heading><Heading> NIPPON FOODS</Heading>
+                    <Flex justifyContent='center'><Button mt='16px' color='white' borderRadius='3xl' bgColor='rgba(255, 105, 40, 1)'>ORDER NOW</Button></Flex>
+                </Flex>     
+
+            </Flex>
+
+            <Box>
+                <Heading textAlign='center'>MOST POPULAR CHEFS</Heading>
+                <Flex flexWrap='wrap' justifyContent='space-evenly'>
+                {chefs.map(chef=><ChefCard key={chef.id} chef={chef}></ChefCard>)}
+                </Flex>
+            </Box>
+
         </div>
     );
 };
--- src/components/chefs/ChefCard.jsx
+++ src/components/chefs/ChefCard.jsx
@@ -1,10 +1,28 @@
+import { Box, Button, Card, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ChefCard = () => {
+const ChefCard = ({ chef }) => {
+    const { name, likes, image, number_of_recipes, years_of_experience, id } = chef
     return (
-        <div>
+        <Flex bgColor='rgba(217, 217, 217, 1)' textAlign='center' w='199px' p='23px' flexDir='column' borderRadius='full' my='30px'>
+            <Image borderRadius='full' h='auto' w='full' src={image}></Image>
+            <Heading fontSize='16px' my='20px'>{name}</Heading>
+            <Box p='12px'  color='white' borderRadius='3xl' bgColor='rgba(255, 105, 40, 1)'><Text>Total Recipes</Text>
+                <Heading fontSize='19px'>{number_of_recipes}</Heading>
+            </Box>
+            <Flex  mt='10px' gap='10px'>
+                <Box w='full' p='12px'  color='white' borderRadius='3xl' bgColor='rgba(255, 105, 40, 1)'>
+                    <Heading fontSize='19px'>👍 {likes}</Heading>
+                </Box>
+                <Box w='full'  p='12px'  color='white' borderRadius='3xl' bgColor='rgba(255, 105, 40, 1)'><Text>{years_of_experience} Years</Text>
+                    <Heading fontSize='9px'>Experience</Heading>
+                </Box>
+            </Flex>
+            <Link to={`chef/${id}`}><Button mb='15px' mt='16px' p='25px' color='black' borderRadius='xl' bgColor='rgba(255, 105, 40, 1)'>VIEW RECIPES</Button></Link>
+
             
-        </div>
+        </Flex>
     );
 };
 
